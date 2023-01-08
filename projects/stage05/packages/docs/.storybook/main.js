352c1ae1e982e5a29b577f33a07fca3c0d319799
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-08 10:28:57 -0300

    add workflow

--- projects/stage05/packages/docs/.storybook/main.js
+++ projects/stage05/packages/docs/.storybook/main.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/Estudos-Ignite-React-005/'
     }
 
     return config
