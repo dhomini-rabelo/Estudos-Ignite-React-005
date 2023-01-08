889bc2d4ea6ee5fb87bbd525339c7655bff9dde5
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-07 22:25:32 -0300

    add Button

--- projects/stage05/packages/react/dist/index.mjs
+++ projects/stage05/packages/react/dist/index.mjs
@@ -218,9 +218,82 @@ function Avatar2(props) {
     /* @__PURE__ */ jsx(AvatarFallback, { delayMs: 600, children: /* @__PURE__ */ jsx(User, {}) })
   ] });
 }
+
+// src/components/Button.tsx
+var Button = styled("button", {
+  all: "unset",
+  borderRadius: "$sm",
+  fontSize: "$sm",
+  fontWeight: "$medium",
+  fontFamily: "$default",
+  textAlign: "center",
+  minWidth: 120,
+  boxSizing: "border-box",
+  padding: "0 $4",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "$2",
+  cursor: "pointer",
+  svg: {
+    width: "$4",
+    height: "$4"
+  },
+  "&:disabled": {
+    cursor: "not-allowed"
+  },
+  variants: {
+    variant: {
+      primary: {
+        color: "$white",
+        background: "$ignite-500",
+        "&:not(:disabled):hover": {
+          background: "$ignite-300"
+        },
+        "&:disabled": {
+          backgroundColor: "$gray-200"
+        }
+      },
+      secondary: {
+        color: "$ignite-300",
+        border: "2px solid $ignite-500",
+        "&:not(:disabled):hover": {
+          background: "$ignite-500",
+          color: "$white"
+        },
+        "&:disabled": {
+          color: "$gray-200",
+          borderColor: "$gray-200"
+        }
+      },
+      tertiary: {
+        color: "$gray-100",
+        "&:not(:disabled):hover": {
+          color: "$white"
+        },
+        "&:disabled": {
+          color: "$gray-600"
+        }
+      }
+    },
+    size: {
+      sm: {
+        height: 38
+      },
+      md: {
+        height: 46
+      }
+    }
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "md"
+  }
+});
 export {
   Avatar2 as Avatar,
   Box,
+  Button,
   Heading,
   Text
 };
---
+++ projects/stage05/packages/react/src/components/Button.tsx
@@ -0,0 +1,93 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  all: 'unset',
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+  minWidth: 120,
+  boxSizing: 'border-box',
+  padding: '0 $4',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  cursor: 'pointer',
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        color: '$white',
+        background: '$ignite-500',
+
+        '&:not(:disabled):hover': {
+          background: '$ignite-300',
+        },
+
+        '&:disabled': {
+          backgroundColor: '$gray-200',
+        },
+      },
+
+      secondary: {
+        color: '$ignite-300',
+        border: '2px solid $ignite-500',
+
+        '&:not(:disabled):hover': {
+          background: '$ignite-500',
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray-200',
+          borderColor: '$gray-200',
+        },
+      },
+
+      tertiary: {
+        color: '$gray-100',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray-600',
+        },
+      },
+    },
+
+    size: {
+      sm: {
+        height: 38,
+      },
+
+      md: {
+        height: 46,
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+  as?: ElementType
+}
